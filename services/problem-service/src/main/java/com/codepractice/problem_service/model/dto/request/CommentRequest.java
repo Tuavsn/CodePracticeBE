41e4a934d9cb30016a123562f29433b5150e8854
Tuavsn <hoctuavsn@gmail.com>
2025-08-12 15:48:19 +0700

    [Fix]: Remove @Pattern validation from DTOs

--- services/problem-service/src/main/java/com/codepractice/problem_service/model/dto/request/CommentRequest.java
+++ services/problem-service/src/main/java/com/codepractice/problem_service/model/dto/request/CommentRequest.java
@@ -1,7 +1,6 @@
 package com.codepractice.problem_service.model.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -14,7 +13,6 @@ import lombok.NoArgsConstructor;
 @Builder
 public class CommentRequest {
     @NotBlank(message = "Problem ID is required")
-    @Pattern(regexp = "^[0-9a-fA-F]{8}\\-[0-9a-fA-F]{4}\\-[0-9a-fA-F]{4}\\-[0-9a-fA-F]{4}\\-[0-9a-fA-F]{12}$", message = "Problem ID must be a valid UUID")
     private String problemId;
 
     @NotBlank(message = "Content is required")
